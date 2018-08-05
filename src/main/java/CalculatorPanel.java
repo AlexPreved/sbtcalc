2623daa0c24f14b6b7d75d6ad9bc817f10d854ac
Alex <t89137899903@gmail.com>
2018-08-05 21:53:51 +0300

    1) Basic calc

--- src/main/java/CalculatorPanel.java
+++ src/main/java/CalculatorPanel.java
@@ -1,5 +1,118 @@
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-public class CalculatorPanel {
+/**
+ * Панель с кнопками и областью отображения результатов вычислений
+ */
 
+public class CalculatorPanel extends JPanel {
+    private JButton display;
+    private JPanel panel;
+    private double result;
+    private String lastCommand;
+    private boolean start;
+
+    public CalculatorPanel() {
+        setLayout(new BorderLayout());
+        result = 0;
+        lastCommand = "=";
+        start = true;
+        // ввести область отображения результатов вычислений
+        display = new JButton("0");
+        display.setEnabled(false);
+        add(display, BorderLayout.NORTH);
+
+        ActionListener insert = new InsertAction();
+        ActionListener command = new CommandAction();
+
+        //ввести кнопки сеткой 4х4
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(4, 4)); // <-- смени на GridBagLayout (12.6.1- GridBagConstraints)
+
+        addButton("7", insert);
+        addButton("8", insert);
+        addButton("9", insert);
+        addButton("/", command);
+
+        addButton("4", insert);
+        addButton("5", insert);
+        addButton("6", insert);
+        addButton("*", command);
+
+        addButton("1", insert);
+        addButton("2", insert);
+        addButton("3", insert);
+        addButton("-", command);
+
+        addButton("0", insert);
+        addButton(".", insert);
+        addButton("=", command);
+        addButton("+", command);
+
+        add(panel, BorderLayout.CENTER);
+    }
+
+    /**
+     * Ввод кнопки на центральной панели
+     *
+     * @param label    - Метка кнопки
+     * @param listener - Слушатель действий кнопки
+     */
+    private void addButton(String label, ActionListener listener) {
+        JButton button = new JButton(label);
+        button.addActionListener(listener);
+        panel.add(button);
+
+    }
+
+    /**
+     * При обработке событий строка действия кнопки вводится
+     * в конце отображаемого текста
+     */
+    private class InsertAction implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            String input = event.getActionCommand();
+            if (start) {
+                display.setText("");
+                start = false;
+            }
+            display.setText(display.getText() + input);
+        }
+    }
+
+    /**
+     * При обработке событий выполняется команда,
+     * указанная в строке действия кнопки
+     */
+    private class CommandAction implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            String command = event.getActionCommand();
+
+            if (start) {
+                if (command.equals("-")) {
+                    display.setText(command);
+                    start = false;
+                } else lastCommand = command;
+            } else {
+                calculate(Double.parseDouble(display.getText()));
+                lastCommand = command;
+                start = true;
+            }
+        }
+    }
+
+    /**
+     * Выполняет ожидающую калькуляцию
+     *
+     * @param x Значение, накапливаемое с учетом предыдущего рпзультата
+     */
+    public void calculate(double x) {
+        if (lastCommand.equals("+")) result += x;
+        else if (lastCommand.equals("-")) result -= x;
+        else if (lastCommand.equals("*")) result *= x;
+        else if (lastCommand.equals("/")) result /= x;
+        else if (lastCommand.equals("=")) result = x;
+        display.setText(" " + result);
+    }
 }
